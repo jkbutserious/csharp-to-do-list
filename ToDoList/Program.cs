502a29042c72bf7954f9bf755e4383dbab811506
Jeff Kim <kim2jy@hotmail.com>
2020-07-15 11:46:25 -0700

    Begin adding UI in the Program class
    Co-Authored-By: Taylor Phillips <taylorphillips133@gmail.com>

---
+++ ToDoList/Program.cs
@@ -0,0 +1,13 @@
+using ToDoList.Models;
+using System;
+
+namespace ToDoList
+{
+    class Program
+    {
+      public static void Main()
+      {
+        Console.WriteLine("test");
+      }
+    }
+}
